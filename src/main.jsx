453dafab7c2ce442a2ae7335265e30135afee7aa
Khabbab Hossen <sk.khabbab50@gmail.com>
2025-11-08 23:31:04 +0600

    ecoTrackClient site project setup done

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from "./layout/RootLayout";
+import Challenges from "./Pages/Challenges/Challenges";
+import Home from "./Pages/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/challenges",
+        element: <Challenges />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
